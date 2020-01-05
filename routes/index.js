a61a9dcf95cbbb2b3a543a4287e9f6e5e8e8b974
iNoyoka <as1232161@gmail.com>
2020-01-05 15:09:20 +0800

    searchCourse

--- routes/index.js
+++ routes/index.js
@@ -164,7 +164,7 @@ router.post('/home/listPersonalCourse',function(req,res,next){
 				idlist.push(result[i].studentcourse_name);
 			}
 			session
-				.run("match (c:Course) match (c)-->(p:Provider) match (a:Author)-->(c) where c.idx in "+idlist+" return distinct c.Title AS `coursename`, a.author AS `professor`")
+				.run("match (c:Course) match (c)-->(p:Provider) match (a:Author)-->(c) where c.idx in ["+idlist+"] return distinct c.Title AS `coursename`, a.author AS `professor`")
 				.then(result2 => {
 					console.log(result2.records[0].get('coursename'));
 					//obj.coursename = result2.records[0].get('coursename');
