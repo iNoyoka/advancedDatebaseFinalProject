9f350ad4f1c3ad3d6e70c33408e81bf05b7c2d6c
iNoyoka <as1232161@gmail.com>
2020-01-05 16:00:25 +0800

    searchCourse

--- routes/index.js
+++ routes/index.js
@@ -170,8 +170,7 @@ router.post('/home/listPersonalCourse',function(req,res,next){
 					var temp_i = 0;
 					result2.records.forEach(function (record) {
 						console.log(record.get('coursename')+" - "+record.get('professor'));
-						if(anoidlist.includes(record.get('id')){
-							console.log('repeat');
+						if(anoidlist.includes(record.get('id'))){
 						}
 						else{
 							anoidlist.push(record.get('id'));
